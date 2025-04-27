6b8a7089cd90a253b35566664416e9e310662cf3
pank-su <frostkslo1@yandex.ru>
2025-04-27 11:39:34 +0300

    :rocket: добавлена публикация

--- composeApp/src/wasmJsMain/kotlin/su/pank/ecards/main.kt
+++ composeApp/src/wasmJsMain/kotlin/su/pank/ecards/main.kt
@@ -3,9 +3,19 @@ package su.pank.ecards
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.url.URLSearchParams
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+
+    val query = window.location.search
+
+    val params = URLSearchParams(query as JsAny)
+
+    println(params) // делаем запрос
+
+
     ComposeViewport(document.body!!) {
         App()
     }
